1af46245174caf9a23e4eda79a75bf1b67073c9f
ArushiJ0 <arushi.123253@gmail.com>
2025-08-24 11:42:01 +0530

    Basic_Keywords

---
+++ Strings/Basic_Keywords.java
@@ -0,0 +1,11 @@
+public class Basic_Keywords {
+public static void main(String[] args){
+String string = "I am studying in SRM University";
+System.out.println("The string contains SRM:"+string.contains("SRM"));
+System.out.println("Length of the string is:"+string.length());
+System.out.println("Substring of the string is:"+string.substring(5,14));
+System.out.println("String in uppercase:"+string.toUpperCase());
+System.out.println("New string:"+string.replace("SRM","VIT"));
+}
+}
+
